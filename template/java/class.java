f4afed490e2ef26df356ebb02e079bcb0ad88369
silvern1990 <silvern1990@gmail.com>
2023-09-07 16:26:35 +0900

    update template support for test package

--- template/java/class.java
+++ template/java/class.java
@@ -3,7 +3,7 @@
  * created: {{_lua:os.date('%y.%m.%d %H:%M')_}}
  */
 
-package {{_lua:vim.fn.expand("%:p"):sub(vim.fn.expand('%:p'):find('/src/main/java/'), -1):gsub('/src/main/java/', ''):gsub('/' .. vim.fn.expand('%:t'), ''):gsub('/', '.')_}};
+package {{_lua:vim.fn.expand("%:p"):sub(vim.fn.expand('%:p'):find('/src/.*/java/'), -1):gsub('/src/.*/java/', ''):gsub('/' .. vim.fn.expand('%:t'), ''):gsub('/', '.')_}};
 
 public class {{_file_name_}} {
 

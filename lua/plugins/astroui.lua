7e4f54306a2816432f352788c357a3b0a73375d9
silvern1990 <silvern1990@gmail.com>
2025-07-09 00:00:44 +0900

    NeoTree highlight setting

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -16,10 +16,23 @@ return {
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
+
+        NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
+        NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
+        NeoTreeTabActive = { bg = "NONE", ctermbg = "NONE" },
+        NeoTreeTabInactive = { bg = "NONE", ctermbg = "NONE" },
+        NeoTreeWinSeparator = { bg = "NONE", ctermbg = "NONE" },
+        NeoTreeTabSeparatorActive = { bg = "NONE", ctermbg = "NONE" },
+        NeoTreeTabSeparatorInactive = { bg = "NONE", ctermbg = "NONE" },
+
         LineNr = { bg = "NONE", ctermbg = "NONE"},
-        Normal = { bg = "None", ctermbg = "NONE" },
+        Normal = { bg = "NONE", ctermbg = "NONE" },
         NormalNC = { bg = "NONE", ctermbg = "NONE"},
         EndOfBuffer = { bg = "NONE", ctermbg = "NONE"},
+        TabLine = { bg = "NONE", ctermbg = "NONE" },
+        TabLineSel = { bg = "NONE", ctermbg = "NONE" },
+        TabLineFill = { bg = "NONE", ctermbg = "NONE" },
+
       },
       astrodark = { -- a table of overrides/changes when applying the astrotheme theme
         -- Normal = { bg = "#000000" },

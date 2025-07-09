a06725b82a28e33d57f4eea4c895f5c6c97eab9b
silvern1990 <silvern1990@gmail.com>
2025-07-09 14:06:28 +0900

    override jdtls settings

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -118,53 +118,25 @@ return {
   -- jdtls setting
   {
     "mfussenegger/nvim-jdtls",
-    config = function(_, opts)
-      -- setup autocmd on filetype detect java
-      vim.api.nvim_create_autocmd("Filetype", {
-        pattern = "java", -- autocmd to start jdtls
-        callback = function()
-          if opts.root_dir and opts.root_dir ~= "" then
-            require("jdtls").start_or_attach(opts)
-            require("todo-comments").setup()
-          else
-            require("astrocore").notify("jdtls: root_dir not found. Please specify a root marker", vim.log.levels.ERROR)
-          end
-        end,
-      })
-
-      vim.api.nvim_create_autocmd("BufWritePost", {
-        pattern = { "*.css", "*.html", "*.js", "*.xml" },
-        callback = function() -- auto build for thymeleaf template
-          local root_dir = require("jdtls.setup").find_root { "mvnw", ".git", "gradlew", "pom.xml", "build.gradle" }
-          if root_dir and root_dir ~= "" then
-            local Job = require "plenary.job"
-            Job:new({
-              command = "./mvnw",
-              args = { "compile" },
-              cwd = root_dir,
-            }):start()
-          end
-        end,
-      })
-      -- create autocmd to load main class configs on LspAttach.
-      -- This ensures that the LSP is fully attached.
-      -- See https://github.com/mfussenegger/nvim-jdtls#nvim-dap-configuration
-      vim.api.nvim_create_autocmd("LspAttach", {
-        pattern = "*.java",
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          -- ensure that only the jdtls client is activated
-          if client.name == "jdtls" then
-            local vmArgs = "-Djava.net.preferIPv4Stack=true"
-            require("jdtls.dap").setup_dap_main_class_configs {
-              config_overrides = {
-                vmArgs = vmArgs,
-              },
+    opts = {
+      settings = {
+        java = {
+          completion = {
+            favoriteStaticMembers = {
+              "org.hamcrest.MatcherAssert.assertThat",
+              "org.hamcrest.Matchers.*",
+              "org.hamcrest.CoreMatchers.*",
+              "org.junit.jupiter.api.Assertions.*",
+              "java.util.Objects.requireNonNull",
+              "java.util.Objects.requireNonNullElse",
+              "org.mockito.Mockito.*",
+              "org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*",
+              "org.springframework.test.web.servlet.result.MockMvcResultMatchers.*",
             }
-          end
-        end,
-      })
-    end,
+          }
+        }
+      }
+    }
   },
 
   -- dap ui
